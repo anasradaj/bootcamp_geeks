8044dcce9368488e47dcf721d2a0a9d789521a82
anasradaj <anasradaj@gmail.com>
2025-05-07 11:26:30 +0100

    daily challenge done

--- week1/day1/daily-challenges/daily-challenge.py
+++ week1/day1/daily-challenges/daily-challenge.py
@@ -1,1 +1,24 @@
 #Challenge 1 :
+number= int(input("Enter a number: "))
+length= int(input("Enter a length of the number: "))
+multiples= [number * i for i in range(1, length + 1)]
+print(f"Given {number} and {length}, the multiples are: {multiples}")
+
+
+#Challenge 2:
+word = input("Enter a word: ")
+word2 = ""
+
+for i in range(len(word)):
+    if i > 0 and word[i] == word[i-1]:
+        word2 += ""
+    else: 
+        word2 += word[i]
+
+print(word2)
+
+
+
+
+
+       
