a3f1bcbdbe63fb9859fb7af64c04d980028728a7
anasradaj <anasradaj@gmail.com>
2025-05-20 20:47:08 +0100

    add daily challenge stars

--- week3/day1/daily-challenges/daily_challenge_notBad.js
+++ week3/day1/daily-challenges/daily_challenge_notBad.js
@@ -1,6 +1,5 @@
 // Daily Challenge: Not Bad
 const sentence = "The song is not that bad, I love it";
-
 const wordNot = sentence.indexOf("not");
 const wordBad = sentence.indexOf("bad");
 
@@ -9,4 +8,15 @@ if (wordNot !== -1 && wordBad !== -1 && wordBad > wordNot) {
     console.log(result);
 } else {
     console.log(sentence);
+}
+
+const sentence2 = "The movie is bad, it is not good";
+const wordNot2 = sentence2.indexOf("not");
+const wordBad2 = sentence2.indexOf("bad");
+
+if (wordNot2 !== -1 && wordBad2 !== -1 && wordBad2 > wordNot2) {   
+    const result = sentence2.slice(0, wordNot2) + "good" + sentence2.slice(wordBad2 + 3);
+    console.log(result);
+} else {
+    console.log(sentence2);
 }
---
+++ week3/day1/daily-challenges/daily_challenge_stars.js
@@ -0,0 +1,18 @@
+// // Daily challenge: Stars
+let star = "*"
+for (let i = 0; i < 6; i++) {
+    console.log(star);
+    star += "*";
+}
+
+
+let star1 = "*"
+for (let i = 0; i < 3; i++) {
+    console.log(star1);
+    star1 += "*";
+    for (let j = 0; j < 1; j++) {
+        console.log(star1);
+        star1 += "*";
+    }    
+}
+
