f564993f8888cab47fedd0bb50942ff09622acc0
anasradaj <anasradaj@gmail.com>
2025-05-26 17:04:37 +0100

    Add exercises xp 1 to 5

---
+++ week4/day1/exercises/exercises_xp/exercise1.js
@@ -0,0 +1,70 @@
+// #1
+function funcOne() {
+    let a = 5;
+    if(a > 1) {
+        a = 3;
+    }
+    alert(`inside the funcOne function ${a}`);
+}
+
+// #1.1 - run in the console:
+funcOne()
+// #1.2 What will happen if the variable is declared 
+// with const instead of let ? : const are variables whose values cannot be reassigned after their initial declaration. any subsequent attempt to change its value will result in an error.
+
+//#2
+let a = 0;
+function funcTwo() {
+    a = 5;
+}
+
+function funcThree() {
+    alert(`inside the funcThree function ${a}`);
+}
+
+// #2.1 - run in the console:
+funcThree()
+funcTwo()
+funcThree()
+// #2.2 What will happen if the variable is declared 
+// with const instead of let ? : const variables cannot be reassigned, so funcTwo would throw an error when trying to assign a new value to a.
+
+
+//#3
+function funcFour() {
+    window.a = "hello";
+}
+
+
+function funcFive() {
+    alert(`inside the funcFive function ${a}`);
+}
+
+// #3.1 - run in the console:
+funcFour() // sets a property named a on the window object and assigns it the string value "hello"
+funcFive() // since a is now a property of the window object, it will alert "hello"
+
+//#4
+let a = 1;
+function funcSix() {
+    let a = "test";
+    alert(`inside the funcSix function ${a}`);
+}
+
+
+// #4.1 - run in the console:
+funcSix()
+// #4.2 What will happen if the variable is declared 
+// with const instead of let ? The global constant a (which is 1) remains unaffected. The const declaration inside the function creates a completely separate, locally scoped constant variable with the same name. 
+
+//#5
+let a = 2;
+if (true) {
+    let a = 5;
+    alert(`in the if block ${a}`);
+}
+alert(`outside of the if block ${a}`);
+
+// #5.1 - run the code in the console
+// #5.2 What will happen if the variable is declared 
+// with const instead of let ? In this specific code, neither the global nor the block-scoped variable is reassigned, so using const doesn't alter the output.
